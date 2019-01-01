6e531a2aa1a76f20e3a795a15e965baa888662e7
Kim Nguyen <kimchimik@gmail.com>
2019-01-01 07:22:43 +0000

    intial commits

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./install_depenencies.sh
+./install_vs_code.sh
+./install_docker.sh
---
+++ install_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Clones Git Repository
+https://github.com/KimVNguyen/crostini-docker.git
+cd crostini-docker
+./install.sh
+cd ..
