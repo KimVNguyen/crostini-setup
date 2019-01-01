68c58dd1ea2300b8bc9722524ab420c84ca0c8aa
Kim Nguyen <kimchimik@gmail.com>
2019-01-01 07:54:23 +0000

    fixed formatting

--- install_depenencies.sh
+++ install_depenencies.sh
@@ -4,5 +4,5 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y build-essential
 
-  git config --global user.email "$1"
-  git config --global user.name "$2"
+git config --global user.email "$1"
+git config --global user.name "$2"
